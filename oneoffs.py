155825fff1ee17aea99330ab82897a0933090884
KristofaJosh <christopherjoshua18@gmail.com>
2020-01-09 19:07:53 +0100

    positional arg and keyword func

--- oneoffs.py
+++ oneoffs.py
@@ -8,3 +8,13 @@ def divide(a, b, floor=True):
     except TypeError:
         return f'{a , b} is type {type(a)} and {type(b)} use numbers only'
 
+
+def positional_keyword_argument(*args, **kwargs):
+    try:
+        print('args total value is: ',sum(list(args)))
+        print('kwargs total value is: ',{'Total': sum([values for key, values in kwargs.items()])})
+    except Exception:
+        print('PASSED VALUES ARE NOT OF TYPE INT')
+
+
+positional_keyword_argument(1, 2, 3, 4, a=1, b=9, c=3)
