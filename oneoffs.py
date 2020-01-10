02cf285dea1c81e8c838b593621bd4981bdc246f
KristofaJosh <christopherjoshua18@gmail.com>
2020-01-10 19:16:39 +0100

    created oneoffs.py file

--- oneoffs.py
+++
@@ -1,9 +0,0 @@
-def divide(a, b, floor=True):
-    """return true or floor division"""
-    try:
-        if floor:
-            return a // b
-        else:
-            return a / b
-    except TypeError:
-        return f'{a , b} is type {type(a)} and {type(b)} use numbers only'
