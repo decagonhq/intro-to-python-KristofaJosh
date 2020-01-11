e7f86d2d62a5d4d9c41760645d2e7911d9d3243b
KristofaJosh <christopherjoshua18@gmail.com>
2020-01-11 21:01:09 +0100

    exercise 5 completed

--- oneoffs.py
+++ oneoffs.py
@@ -9,4 +9,14 @@ def divide(a, b, floor=True):
         raise TypeError('unsupported operand type, use numbers of type int or float')
 
 
-divide('a', 5, False)
+divide(2, 5, False)
+
+
+def positional_keyword_argument(*args, **kwargs):
+    try:
+        print('args total value is: ', sum(list(args)))
+        print('kwargs total value is: ', {'Total': sum([values for key, values in kwargs.items()])})
+    except Exception:
+        print('PASSED VALUES ARE NOT OF TYPE INT')
+
+positional_keyword_argument(1, 2, 3, 4, a=1, b=9, c=3)
